5b26c571e1119e0e11b4f00ee92be6da10b042d8
Woonaseo <66362366+Woonaseo@users.noreply.github.com>
2020-07-02 18:02:10 +0900

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,251 @@
+# money = True
+# if money :
+#     print("택시를 타고 가라")
+
+# else:
+#     print("걸어 가라")
+
+# money = Trueif money:
+#     print("택시")
+# print("타고")
+#     print("가라")
+
+
+
+# money = 2000
+# card = True
+# if money >= 3000 or card:
+#     print("택시를 타고 가라")
+
+# else:
+#     print("걸어가라")
+
+
+
+# 1 in [1, 2, 3]
+# 1 not in [1,2,3]
+
+
+# 'a' in ('a', 'b' , 'c')
+
+# 'j' not in 'python'
+
+
+# pocket = ['paper', 'cellphon', 'money']
+# if 'money' in pocket:
+#     print("택시를 타고 가라")
+
+# else:
+#     print("걸어가라")
+
+
+
+
+# pocket = ['paper', 'cellphon', 'money']
+# if 'money' in pocket:
+#     pass 
+# else:
+#     print("카드를 꺼내라")
+
+
+
+
+# pocket = ['paper', 'handphone']
+# card = R
+# \
+
+
+
+
+# message = "success" if score >= 60 else "failure"
+
+
+
+
+# treeHit = 0
+# while treeHit < 10:
+#     treeHit = treeHit +1
+#     print("나무를 %d 번 찍었습니다"%treeHit)
+    
+#     if treeHit == 10:
+#         print("나무가 넘어갑니다.")
+
+
+
+# primpt = """
+# 1. Add
+# 2. Del
+# 3. List
+# 4. Quit
+
+
+# Enter number"""
+
+
+
+
+# number = 0
+# while number != 4:
+#      print(prompt)
+#      number = int(input())
+
+
+# 1. Add
+# 2. Del
+# 3. List
+# 4. Quit
+
+# Enter number:
+
+
+
+
+
+# coffee = 10
+# money = 300
+# while money :
+#     print("돈을 받았으니 커피를 줍니다.")
+#     coffee = coffee -1
+#     print("남은 커피의 양은 %d개입니다" % coffee)
+#     if coffee == 0:
+#         print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+#         break
+
+
+# a = 0
+# while a <10 :
+#     a = a + 1
+
+#     if a %2 ==0 : continue
+#     print(a,end='')
+
+
+# while True:
+# #     print("Ctrl+c 를 눌러야 while문을 빠져나갈 수 있습니다")
+
+
+
+
+# # test_list = ['one', 'two' , 'three']
+# # for i in test_list:
+# #     print(i)
+
+
+
+
+
+# # a = [(1,2),(3,4),(5,6)]
+# # for (first,last) in a:
+# #     print(first + last)
+
+
+
+# # marks = [90,25,67,45,80]
+
+# # number = 0
+
+# # for mark in marks:
+# #     number = number + 1
+# #     if mark >= 60:
+# #         print("%d번 학생은 합격입니다." %number)
+# #     else:
+# #         print("%d번 학생은 불합격입니다."%number)
+
+
+
+# # marks = [90,25,67,45,80]
+
+# # number = 0
+
+# # for mark in marks:
+# #     number = number + 1
+# #     if mark <60:
+# #         continue
+# #     print("%d번 학생은 축하합니다. 합격입니다."%number)
+
+
+
+
+# # a= range(10)
+# # a
+
+# # add = 0 
+# # for i in range(1, 11): 
+# #      add += i 
+
+# # print(add)
+
+
+
+
+
+
+# a = [1,2,3,4]
+# result = []
+# for num in a:
+#     result.append(num*3)
+
+# print(result)
+
+
+
+
+
+
+# a = [1,2,3,4]
+# result = [num * 3 for num in a]
+# print(result)
+
+
+
+# shirt
+
+
+# a=1
+# b=0
+# while a<1001 :
+#     if a%3 == 0 :
+#         b+=a
+        
+#     else:
+#         a +=1
+# print(b)
+
+
+# a=0
+# while a<6:
+#     print('*'*a)
+#     a+=1
+
+
+
+# i = 0
+
+# while True:
+#     i+=1
+#     if i >5: break
+#     print('*'*i)
+
+
+m=[70,60,55,75,95,90,80,80,85,100]
+a=0
+c=len(m)
+
+for num in m:
+    a+=num
+ 
+print(a//c)
+
+
+
+
+
+
+
+
+
+
+# a=0
+
+# for num in range(1,101):
+#     print(num,end='')
